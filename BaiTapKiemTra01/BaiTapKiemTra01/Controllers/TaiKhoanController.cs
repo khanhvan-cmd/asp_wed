0900175c59238d2ca3bf381d0b1f9c2904ff48c0
Vân Nguyễn Thị Khánh <1822040972@dntu.edu.vn>
2024-09-10 15:03:56 +0700

    baitap

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,34 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (model.Username != null)
+            {
+                return Content(model.Username);
+            }
+            return View();
+        }
+        public IActionResult DangNhap(SanPhamViewModel model)
+        {
+            var sanpham = new SanPhamViewModel()
+            {
+                TenSanPham = "Iced Americano",
+                GiaBan = 15000000, // 15 million VND
+                AnhMoTa  = "img/iced-americano.png"
+            };
+           
+            return View(sanpham);
+        }
+
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+    }
+}
