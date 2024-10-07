44fe89f54e649391875e42108ddf651c9049bbbc
Vân Nguyễn Thị Khánh <1822040972@dntu.edu.vn>
2024-10-07 15:41:15 +0700

    baitap

--- BaiKiemTra03_02/BaiKiemTra03_02/obj/Debug/net8.0/BaiKiemTra03_02.AssemblyInfo.cs
+++ BaiKiemTra03_02/BaiKiemTra03_02/obj/Debug/net8.0/BaiKiemTra03_02.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiKiemTra03_02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcff2aa4843738f09e9670c0512c2c6418fcaaf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2fde06b4ed4d267d4a7ddfa57b22873299e309")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiKiemTra03_02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiKiemTra03_02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
